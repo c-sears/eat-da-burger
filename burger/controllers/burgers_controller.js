ae6cec40f1ead7d8537492ab5c1844c6f65f1d47
caleb <caleb.sears22@gmail.com>
2018-06-23 22:53:13 -0400

    removed console.logs

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -8,13 +8,11 @@ const router = express.Router()
 
 router.get('/', (req,res)=>{
     burger.get_burgers(burgers =>{
-        console.log(burgers)
         res.render('index', {burgers:burgers})
     })
 })
 
 router.post('/burger/add', (req,res)=>{
-    console.log(req.body)
     const new_burger = req.body
     burger.add_burger(new_burger, (burgers)=>{
         burger.get_burgers(burgers=>{
@@ -24,7 +22,6 @@ router.post('/burger/add', (req,res)=>{
 })
 
 router.put('/burger/update', (req,res)=>{
-    console.log(req.body)
     burger.update_burger({devoured:req.body.devoured}, {id:req.body.id}, (burgers)=>{
         burger.get_burgers(burgers =>{
             res.render('index', {burgers:burgers})
@@ -32,8 +29,5 @@ router.put('/burger/update', (req,res)=>{
     })
 })
 
-router.get('/test',(req,res)=>{
-    res.json()
-})
 
 module.exports = router
